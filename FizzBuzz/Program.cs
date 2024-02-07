a4cf119671ea8ed40b44f8a168cf8bb308eb235d
daweekosky <dawid_czarnecki@o2.pl>
2024-02-07 10:09:56 +0100

    Generated code for FizzBuzz app

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿string text;
+for (int i = 1; i <= 100; i++)
+{
+    text = "";
+    if (i % 3 == 0)
+    {
+        text += "Fizz";
+    }
+    if (i % 5 == 0)
+    {
+        text += "Buzz";
+    }
+    if (i % 3 != 0 && i % 5 != 0)
+    {
+        text += i;
+    }
+    Console.WriteLine(text);
+}
